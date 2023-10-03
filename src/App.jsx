5f5b22dafbfb4b5e54e66c796684712dff52b7a6
danieldevcode <danieldevcode@gmail.com>
2023-10-03 14:52:06 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <div className="app-container">
+      <p>App</p>
+    </div>
+  );
+}
+
+export default App;
